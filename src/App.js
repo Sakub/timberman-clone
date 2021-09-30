117bf2faa59f3ed43e661f6009b83c605243cb27
Sakub <sandomierzj@gmail.com>
2021-10-01 01:31:12 +0200

    Handle changing directions
    Add sprites
    Change score's text color and size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Score from './components/Score.js';
+import Player from './components/Player.js';
 
 function App() {
 	const [points, setPoints] = useState(0);
+	const player = useRef(null);
+
+	const DIRECTIONS = {
+		LEFT: 'player--left',
+		RIGHT: 'player--right',
+	};
+
+	function increaseScore() {
+		setPoints(currPoints => currPoints + 1);
+	}
+
+	function movePlayer(direction) {
+		player.current.classList.remove(DIRECTIONS.LEFT);
+		player.current.classList.remove(DIRECTIONS.RIGHT);
+
+		player.current.classList.add(direction);
+		increaseScore();
+	}
+
+	function useKey(key, cb) {
+		const callbackRef = useRef(cb);
+
+		useEffect(() => {
+			callbackRef.current = cb;
+		});
+
+		useEffect(() => {
+			function handle(event) {
+				if (event.code === key) {
+					callbackRef.current(event);
+				}
+			}
+			document.addEventListener('keydown', handle);
+			return () => document.removeEventListener('keydown', handle);
+		}, [key]);
+	}
+
+	useKey('KeyA', () => movePlayer(DIRECTIONS.LEFT));
+	useKey('KeyL', () => movePlayer(DIRECTIONS.RIGHT));
+
 	return (
 		<div className='App'>
 			<Score points={points} />
+			<Player playerRef={player} />
 		</div>
 	);
 }
---
+++ src/components/Player.js
@@ -0,0 +1,5 @@
+const Player = ({ playerRef }) => {
+	return <div className='player player--left' ref={playerRef}></div>;
+};
+
+export default Player;
