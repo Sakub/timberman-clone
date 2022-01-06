dedaeff2e1e91a1a03d640ba4612547d521d98be
SakubDev <sandomierzj@gmail.com>
2022-01-06 16:02:16 +0100

    Implement countdown to game over

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Score from './components/Score.js';
 import Player from './components/Player.js';
 import Tree from './components/Tree.js';
 import GameResetModal from './components/GameResetModal';
+import TimeBar from './components/TimeBar.js';
 
 import './App.css';
 
@@ -17,10 +18,23 @@ function App() {
   const [highscore, setHighscore] = useState(
     parseInt(localStorage.getItem('highscore')) || 0
   );
+
+  const [timeLeft, setTimeLeft] = useState(100);
   const [gameRunning, setGameRunning] = useState(true);
 
   const player = useRef(null);
   const tree = useRef(null);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (gameRunning && timeLeft > 0) {
+        setTimeLeft(timeLeft => {
+          if (timeLeft - 10 === 0) return gameOver();
+          return timeLeft - 10;
+        });
+      }
+    }, 700);
+  }, []);
 
   const gameOver = () => {
     if (points > highscore) {
@@ -34,9 +48,10 @@ function App() {
   };
 
   const resetGame = () => {
-    tree.current.resetTree();
+    if (tree.current) tree.current.resetTree();
     setPoints(0);
     setGameRunning(true);
+    setTimeLeft(100);
   };
 
   const increaseScore = () => {
@@ -54,6 +69,7 @@ function App() {
       (direction === DIRECTIONS.LEFT && nextBranch === 'left_branch')
     )
       return gameOver();
+    if (timeLeft + 10 <= 100) setTimeLeft(timeLeft => timeLeft + 10);
     player.current.classList.remove(DIRECTIONS.LEFT, DIRECTIONS.RIGHT);
 
     player.current.classList.add(direction);
@@ -86,7 +102,10 @@ function App() {
   return (
     <div className='App'>
       {gameRunning ? (
-        <Score points={points} />
+        <div>
+          <TimeBar time={timeLeft} />
+          <Score points={points} />
+        </div>
       ) : (
         <GameResetModal
           score={points}
